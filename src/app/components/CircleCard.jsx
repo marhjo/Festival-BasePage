7ad77575df45f0a2e7a41d9b82b1da54c16dab07
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-11-30 17:47:48 +0100

    cirkler færdig

--- src/app/components/CircleCard.jsx
+++ src/app/components/CircleCard.jsx
@@ -1,8 +1,9 @@
 export default function CircleCard(props) {
   return (
-    <article className="mx-auto">
+    <article className="flex flex-col gap-3 max-w-sm place-items-center text-center mx-auto sm:max-w-xs ">
       <img className=" w-36 rounded-full object-cover aspect-square" src="/ecofriendly.jpg" alt="eco friendly" />
-      <p>tekst tekst tekst</p>
+      <h2 className="font-bold text-lg">Overskrift</h2>
+      <p>Dette er en kort tekst som skal beskrive ovenstående cirkel.</p>
     </article>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
-
+import CircleCard from "./components/CircleCard.jsx"
 
 export default function Home() {
   return (
 <main className="pt-[72px]">
-<div className="pb-20 text-slate-800 bg-slate-100">Hej med dig</div>
-
+<section className="py-10 px-5 flex flex-col gap-16 sm:flex-row sm:gap-10 md:px-20">
+<CircleCard/>
+<CircleCard/>
+<CircleCard/>
+</section>
 </main>
   )
 }
