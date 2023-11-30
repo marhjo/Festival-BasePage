2ce6a5b31fa88372894d4d9adc9675b2d90accb7
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-11-30 20:21:20 +0100

    cirkler færdige og dynamiske

--- src/app/components/CircleCard.jsx
+++ src/app/components/CircleCard.jsx
@@ -1,9 +1,9 @@
 export default function CircleCard(props) {
   return (
     <article className="flex flex-col gap-3 max-w-sm place-items-center text-center mx-auto sm:max-w-xs ">
-      <img className=" w-36 rounded-full object-cover aspect-square" src="/ecofriendly.jpg" alt="eco friendly" />
-      <h2 className="font-bold text-lg">Overskrift</h2>
-      <p>Dette er en kort tekst som skal beskrive ovenstående cirkel.</p>
+      <img className=" w-36 rounded-full object-cover aspect-square" src={"/" + props.image + ".jpg"} alt={props.image} />
+      <h2 className="font-bold text-lg">{props.overskrift}</h2>
+      <p>{props.text}</p>
     </article>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,13 @@
-import CircleCard from "./components/CircleCard.jsx"
+import CircleCard from "./components/CircleCard.jsx";
 
 export default function Home() {
   return (
-<main className="pt-[72px]">
-<section className="py-10 px-5 flex flex-col gap-16 sm:flex-row sm:gap-10 md:px-20">
-<CircleCard/>
-<CircleCard/>
-<CircleCard/>
-</section>
-</main>
-  )
+    <main className="pt-[72px]">
+      <section className="py-10 px-5 flex flex-col gap-16 sm:flex-row sm:gap-10 md:px-20">
+        <CircleCard image="ecofriendly" overskrift="Overskrift" text="Her skal der være en kort tekst som beskriver ovenstående" />
+        <CircleCard image="ecofriendly" overskrift="Overskrift" text="Her skal der være en kort tekst som beskriver ovenstående" />
+        <CircleCard image="ecofriendly" overskrift="Overskrift" text="Her skal der være en kort tekst som beskriver ovenstående" />
+      </section>
+    </main>
+  );
 }
