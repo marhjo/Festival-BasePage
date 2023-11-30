9ac8313b474e461fe8a59658d364fcf057f8bfba
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-11-30 14:26:58 +0100

    header færdig

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import MusikLogo from "./musiklogo";
+import ProgramLogo from "./ProgramLogo";
+import BilletLogo from "./BilletLogo";
+export default function Header() {
+  return (
+    <header>
+      <nav className="flex justify-center gap-6 py-3 fixed w-full top-0 bg-[#13141D]">
+        <Link className="grid place-items-center hover:scale-105" href="/" prefetch={false}>
+          <MusikLogo />
+          Kunstnere
+        </Link>
+        <Link className="grid place-items-center hover:scale-105" href="/" prefetch={false}>
+          <ProgramLogo />
+          Program
+        </Link>
+        <Link className="grid place-items-center hover:scale-105" href="/" prefetch={false}>
+          <BilletLogo />
+          Køb Biletter
+        </Link>
+      </nav>
+    </header>
+  );
+}
