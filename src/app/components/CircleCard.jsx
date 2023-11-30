828ff7317fac399a2fd4588b55a2b0442728bd91
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-11-30 17:00:48 +0100

    circle

---
+++ src/app/components/CircleCard.jsx
@@ -0,0 +1,8 @@
+export default function CircleCard(props) {
+  return (
+    <article className="mx-auto">
+      <img className=" w-36 rounded-full object-cover aspect-square" src="/ecofriendly.jpg" alt="eco friendly" />
+      <p>tekst tekst tekst</p>
+    </article>
+  );
+}
