7f6b2ccd898d227bf192d889b3483df29c724141
katbang <katr164m@stud.kea.dk>
2023-12-06 12:40:03 +0100

    giver timeslots height

--- src/app/components/TimeSlot.jsx
+++ src/app/components/TimeSlot.jsx
@@ -1,6 +1,6 @@
 export default function TimeSlot({ program }) {
   return (
-    <li className=" flex justify-between  mb-8">
+    <li className=" flex justify-between h-20 gap-1 mb-8">
       <h3 className="text-2xl text-secondary">{program.act}</h3>
       <p className="text-2xl text-[#CDD6F4]">
         <span>{program.start}</span>-<span>{program.end}</span>
