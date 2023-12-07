e38ce235b165db4ac40209508e201b6d4c8dce3c
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-12-07 12:55:41 +0100

    små rettelser

--- src/app/components/CircleCard.jsx
+++ src/app/components/CircleCard.jsx
@@ -1,9 +1,9 @@
 export default function CircleCard(props) {
   return (
     <article className="flex flex-col gap-3 max-w-sm place-items-center text-center mx-auto sm:max-w-xs ">
-      <img className=" w-36 rounded-full object-cover aspect-square" src={"/" + props.image + ".jpg"} alt={props.image} />
-      <h2 className="font-bold text-lg">{props.overskrift}</h2>
-      <p>{props.text}</p>
+      <img className=" w-36 md:w-44 rounded-full object-cover aspect-square" src={"/" + props.image + ".jpg"} alt={props.image} />
+      <h2 className="font-bold text-2xl">{props.overskrift}</h2>
+      <p className="text-lg">{props.text}</p>
     </article>
   );
 }
