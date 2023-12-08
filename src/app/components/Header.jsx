0a84d2ac57a7cf6ad482a4033fe078d9d51458fa
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-12-08 11:35:29 +0100

    header opdateret med logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,10 +2,32 @@ import Link from "next/link";
 import MusikLogo from "./musiklogo";
 import ProgramLogo from "./ProgramLogo";
 import BilletLogo from "./BilletLogo";
+import LogoMobile from "./LogoMobile";
+import LogoDesktop from "./LogoDesktop";
 export default function Header() {
   return (
     <header>
-      <nav className="flex justify-center gap-6 sm:gap-10 md:gap-16 py-3 fixed w-full top-0 bg-dark-blue z-50 text-secondary-text">
+      <nav className="flex justify-between align-middle px-3 sm:gap-10 md:gap-16 py-3 fixed w-full top-0 bg-dark-blue z-50 text-secondary-text">
+
+        <div>
+        <Link
+          className="hover:scale-105 sm:hidden"
+          href="/"
+          prefetch={false}
+        >
+          <LogoMobile/>
+        </Link>
+        <Link
+          className="hidden hover:scale-105 sm:block"
+          href="/"
+          prefetch={false}
+        >
+          <LogoDesktop/>
+        </Link>
+    
+      
+        </div>
+        <div className="flex gap-5 sm:gap-11">
         <Link
           className="grid place-items-center hover:scale-105"
           href="/kunstnere"
@@ -29,7 +51,8 @@ export default function Header() {
         >
           <BilletLogo />
           Køb Biletter
-        </Link>
+        </Link></div>
+        <div className="hidden sm:block w-20"></div>
       </nav>
     </header>
   );
