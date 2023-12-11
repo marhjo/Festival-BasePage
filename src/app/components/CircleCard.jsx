42a262303f79519e4f38fe0f58c00b1afe4a7f4a
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-12-11 08:40:38 +0100

    hero banner færdig

--- src/app/components/CircleCard.jsx
+++ src/app/components/CircleCard.jsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 export default function CircleCard(props) {
   return (
     <article className="flex flex-col gap-3 max-w-sm place-items-center text-center mx-auto sm:max-w-xs ">
-      <img className=" w-36 md:w-44 rounded-full object-cover aspect-square" src={"/" + props.image + ".jpg"} alt={props.image} />
+
+      <Image
+      src={"/" + props.image + ".jpg"} 
+      className=" w-36 md:w-44 rounded-full object-cover aspect-square" 
+      width={300}
+      height={300}
+      alt={props.image}
+    />
       <h2 className="font-bold text-2xl">{props.overskrift}</h2>
       <p className="text-lg">{props.text}</p>
     </article>
--- src/app/components/HeroBanner.jsx
+++ src/app/components/HeroBanner.jsx
@@ -1,29 +1,29 @@
-// export default function HeroBanner() {
-//   return (
-//     <section
-//       class="w-full
-//         h-screen
-//         max-h-[32rem]
-//         p-24
-//         relative
-//         overflow-hidden
-//         block
-//         z-10
-                    
-//         bg-[url('festival-banner.jpg')]
-//         bg-cover
-//         bg-no-repeat
-//         bg-center
+import LogoDesktop from "./LogoDesktop";
 
-//         before:content-['']
-//         before:absolute
-//         before:inset-0
-//         before:block
-//         before:bg-gradient-to-r
-//         before:from-green-400
-//         before:to-blue-500
-//         before:opacity-75
-//         before:z-[-5]
-//         "></section>
-//   );
-// }
+export default function HeroBanner() {
+  return (
+    <section
+      className="w-full
+        h-screen
+        sm:max-h-[32rem]
+        max-h-[28rem]
+        grid bg-fixed overflow-hidden
+        md:pr-[50%]
+  
+        "
+      style={{
+        backgroundImage: `url('https://images.unsplash.com/photo-1484494789010-20fc1a011197?q=80&w=2573&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}>
+      <div className=" bg-background bg-opacity-70 h-full pl-16 pr-5 pt-10  ">
+          <img className="w-3/4" src="./foo-festival-logo.svg" alt="" />
+          <p className="text-3xl mt-10">15 januar - 21 januar</p>
+          <p className="text-3xl mb-4">København</p>
+          <p className="max-w-prose text-xl hidden sm:block mb-3">Vi samler 120 kunstnere fra alle af musikkens verdenshjørner og samler dem i en uge med masser af fed stemning og energi</p>
+        </div>
+
+    </section>
+  );
+}
