21b756b2c800b6bc3ade5a242fc62ca6fe6811e7
katbang <katr164m@stud.kea.dk>
2023-12-12 12:56:49 +0100

    config

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "image.unsplash.com",
+        hostname: "images.unsplash.com",
         port: "",
       },
       {
