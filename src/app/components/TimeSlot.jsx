9000253975a5105d265c9a10d1860934c8190cc6
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-12-13 10:49:52 +0100

    program fixet - timeslot på én linje

--- src/app/components/TimeSlot.jsx
+++ src/app/components/TimeSlot.jsx
@@ -1,10 +1,10 @@
 export default function TimeSlot({ program }) {
   return (
-    <li className=" flex justify-between h-20 lg:h-28 xl:h-24 gap-1 mb-8">
-      <h3 className="text-xl sm:text-2xl lg:text-xl xl:text-2xl text-secondary">
+    <li className=" flex justify-between h-20 lg:h-28 xl:h-24 gap-6 mb-8">
+      <h3 className="text-xl  sm:text-2xl lg:text-xl xl:text-2xl text-secondary">
         {program.act}
       </h3>
-      <p className="text-xl sm:text-2xl lg:text-xl xl:text-2xl text-secondary-text">
+      <p className=" flex text-xl sm:text-2xl lg:text-xl xl:text-2xl text-secondary-text">
         <span>{program.start}</span>-<span>{program.end}</span>
       </p>
     </li>
