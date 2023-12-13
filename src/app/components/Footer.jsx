66e8816667914ed3eca7b4683e45ea7f2b6d0418
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-12-13 09:50:38 +0100

    footer med email og tel tag

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -13,25 +13,27 @@ export default function Footer() {
           <Link className="hover:underline" href="/" prefetch={false}>
             Partners
           </Link>
-          <Link className="hover:underline"  href="/" prefetch={false}>
+          <Link className="hover:underline" href="/" prefetch={false}>
             Volunteer
           </Link>
         </nav>
       </div>
       <div className="flex flex-col gap-2">
         <h2 className="font-bold text-accent">Contact us</h2>
-        <div>
-            <p>Tel: 43 23 65 45</p>
-            <p className="mb-4">Mail: kontakt@foofestival.dk</p>
-            <p>Metallicavej 17</p>
-            <p>2700 Brønshøj</p>
+        <div className="flex flex-col">
+          <Link href="tel:+4542367745">Tel: +45 42 36 77 45</Link>
+          <Link href="mailto:contact@foofestival.dk" target="_blank">
+            Mail: contact@foofestival.dk
+          </Link>
+          <p className="mt-4">Metallicavej 17</p>
+          <p>2700 Brønshøj</p>
         </div>
       </div>
       <div className="flex flex-col gap-2 order-first sm:order-3">
         <h2 className="font-bold text-accent hidden sm:block">Follow us</h2>
         <div className="flex gap-5 justify-center">
-            <FacebookLogo/>
-            <InstagramLogo/>
+          <FacebookLogo />
+          <InstagramLogo />
         </div>
       </div>
     </footer>
